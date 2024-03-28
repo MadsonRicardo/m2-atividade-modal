836e5cd71bf773f8d2767fb5e15ac9ecc82f353b
Madson de Miranda <madson_miranda@hotmail.com.br>
2024-03-27 20:34:19 -0400

    update

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,15 @@
-/* Desenvolva seu código aqui... */
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.querySelector('.modalContainer');
+    const openModalButtons = document.querySelectorAll('.form__register-link');
+    const closeModalButton = document.querySelector('.close');
+
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            modal.showModal();
+        });
+    });
+
+    closeModalButton.addEventListener('click', function() {
+        modal.close();
+    });
+});
